7f334d8fda6b2ff2f777b13a9461a194ffdac218
adipokala <adithyapokala@outlook.com>
2019-07-13 23:49:44 +0200

    login page update, models update

--- models/models.py
+++ models/models.py
@@ -11,6 +11,20 @@ class AppAuthData(models.Model):
     class Meta:
         db_table = "app_auth_data"
 
+# Model which stores notification types
+class NotificationType(models.Model):
+    notify_on = models.CharField(max_length = 25)
+
+    class Meta:
+        db_table = "notification_type"
+
+# Model which stores subscription types
+class SubscriptionType(models.Model):
+    subscription = models.CharField(max_length = 25)
+
+    class Meta:
+        db_table = "subscription_type"
+
 # Model for storing user info
 # app_auth_data_id links the user with authentication information
 class UserInfo(models.Model):
@@ -22,12 +36,32 @@ class UserInfo(models.Model):
     last_name = models.CharField(max_length = 25)
     email = models.CharField(max_length = 65, unique = True)
     date_of_birth = models.DateField()
+    subscription_type_id = models.ForeignKey(
+        SubscriptionType,
+        on_delete = models.CASCADE,
+        default = 1
+    )
+    expiry_date = models.DateField(blank = True, null = True)
+    is_active = models.BooleanField(default = True)
 
     class Meta:
         db_table = "user_info"
 
+class UserNotificationType(models.Model):
+    user_info_id = models.ForeignKey(
+        UserInfo,
+        on_delete = models.CASCADE
+    )
+    notification_type_id = models.ForeignKey(
+        NotificationType,
+        on_delete = models.CASCADE
+    )
+
+    class Meta:
+        db_table = "user_notification_type"
+
 class catch_email_temp(models.Model):
     email = models.CharField(max_length = 50)
 
-    class meta:
+    class Meta:
         db_table = "catch_email_temp"
