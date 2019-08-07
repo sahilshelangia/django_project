003d23be8d4b5064fd562e217ac8f914598b1fe2
adipokala <adithyapokala@outlook.com>
2019-08-07 19:37:40 +0200

    addition of entities, updates in business models.

---
+++ business/entities.py
@@ -0,0 +1,21 @@
+# This file contains entities for all the models
+
+class AppAuthDataEntity:
+
+    def __init__(self):
+        self.id = 0
+        self.account_kit_id = ''
+        self.phone_number = ''
+
+class UserInfoEntity:
+
+    def __init__(self):
+        self.id = 0
+        self.app_auth_data_id = 0
+        self.first_name = ''
+        self.last_name = ''
+        self.email = ''
+        self.date_of_birth = ''
+        self.subscription_type_id = 0
+        self.expiry_date = None
+        self.is_active = True
